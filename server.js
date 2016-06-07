8946b8d82eb96a730efb52f709e58abe8b47c99b
rabell <rabell@fanatics.com>
2016-06-07 11:48:23 -0600

    Created Post Todos

--- server.js
+++ server.js
@@ -1,22 +1,18 @@
+var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-  id: 1,
-  description: 'Do something important!',
-  completed: false
-},{
-  id: 2,
-  description: 'Do another important thing!',
-  completed: false
-}];
+var todoNextId = 1;
+var todos = [];
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res){
   res.send("Todo API Root")
 });
 
 app.get('/todos', function(req, res){
-  res.send(todos);
+  res.json(todos);
 });
 
 app.get('/todos/:id', function(req, res){
@@ -32,11 +28,20 @@ app.get('/todos/:id', function(req, res){
   }
 
   if (foundTodo) {
-    res.send(foundTodo);
+    res.json(foundTodo);
   } else {
     res.status(404).send();
   }
 })
+
+app.post('/todos', function(req, res){
+  var body = req.body;
+  body.id = todoNextId;
+  todos.push(body);
+  todoNextId++;
+
+  res.json(body);
+});
 
 app.listen(PORT, function(){
   console.log('Express listeningon port ' + PORT + '!');
